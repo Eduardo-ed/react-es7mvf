09586208b6c660afcb584c502d9b1b81001198e7
Eduardo-ed <ecardenosag01@educantabria.es>
2021-12-14 10:53:57 +0100

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./style.css";
+import Etiqueta1 from "./componets/Etiqueta1";
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>Holas:</h1>
+      <Etiqueta1 nombre="1"/>
     </div>
   );
 }
---
+++ src/componets/Etiqueta1.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Etiqueta1(props) {
+  return <div>Hola {props.nombre}</div>;
+}
