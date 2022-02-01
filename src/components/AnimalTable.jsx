2c0ee1962c271d25fff747ac435f07d0c95211ec
Eduardo-ed <ecardenosag01@educantabria.es>
2022-02-01 12:10:10 +0100

    Tabla animales

--- src/components/AnimalTable.jsx
+++ src/components/AnimalTable.jsx
@@ -1,29 +1,35 @@
 import React from 'react';
-import { Card, Container, Button, Row, Col } from 'react-bootstrap';
+import { Table, Card, Container, Button, Row, Col } from 'react-bootstrap';
 
 class AnimalTable extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      tableData: [],
       selectedItem: '',
     };
     this.change = this.change.bind(this);
+    this.reload = this.reload.bind(this);
   }
 
-  async change(item) {
+  async reload(item) {
     const response = await fetch(
-      'https://zoo-animal-api.herokuapp.com/animals/rand'
+      'https://zoo-animal-api.herokuapp.com/animals/rand/5'
     );
     const responseData = await response.json();
-    this.setState({ selectedItem: responseData });
+    this.setState({tableData: responseData, selectedItem: responseData[0] });
+  }
+
+  change(item) {
+    this.setState({ selectedItem: item });
   }
 
   async componentDidMount() {
     const response = await fetch(
-      'https://zoo-animal-api.herokuapp.com/animals/rand'
+      'https://zoo-animal-api.herokuapp.com/animals/rand/5'
     );
     const responseData = await response.json();
-    this.setState({ selectedItem: responseData });
+    this.setState({ tableData: responseData, selectedItem: responseData[0] });
   }
 
   render() {
@@ -31,6 +37,24 @@ class AnimalTable extends React.Component {
       <Container>
         <h1>Animal Aleatorio</h1>
         <Row>
+        <Col lg={8} md={6}>
+            <Table striped bordered hover size="sm" variant="light">
+              <thead>
+                <tr>
+                  <th>Nombre</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.tableData.map((item) => {
+                  return (
+                    <tr onClick={() => this.change(item)}>
+                      <td>{item.name}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </Table>
+          </Col>
           <Col>
             <Card style={{ width: '18rem' }}>
               <Card.Img
@@ -57,7 +81,7 @@ class AnimalTable extends React.Component {
         <br />
         <Row>
           <Col>
-            <Button variant="primary" type="button" onClick={this.change}>
+            <Button variant="primary" type="button" onClick={this.reload}>
               Nuevo animal
             </Button>
           </Col>
